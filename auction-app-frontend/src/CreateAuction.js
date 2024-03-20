47ce4b01d12e7962d895c481e6b13c2277a4d9ce
Jeremy Callinan <jeremy.callinan@gmail.com>
2024-03-19 20:20:01 -0400

    CreateAuction not complete

---
+++ auction-app-frontend/src/CreateAuction.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+function CreateAuction() {
+const [itemName, setItemName] = useState('');
+const [description, setDescription] = useState('');
+const [endDateTime, setEndDateTime] = useState('');
+const [response, setResponse] = useState('');
+
+const handleAuctionSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+        const result = await axios.post(`http://localhost:3000/auctions`, {
+            itemName, description, endDateTime
+        });
+        setResponse(result.data);
+        if (result.data.success) {
+          alert('Your auction was created successfully, Auction ID: ${result.data.auctionId}')
+        } else {
+            
+        }
+    } catch(error) {
+        console.error('Error saving auction:', error);
+        setErrorMessage('An error occurred while saving the auction.');
+    }
+
+
+}
+
+}
+
+export default CreateAuction;
