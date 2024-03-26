97d00ca6859729e6df229b15cd52dd7615354719
Jeremy Callinan <jeremy.callinan@gmail.com>
2024-03-26 18:58:48 -0400

    Correct routes, nav loads

--- auction-app-frontend/src/App.js
+++ auction-app-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 import AuctionList from './AuctionList';
@@ -8,12 +8,28 @@ import Auction from './Auction';
 function App() {
   return (
     <Router>
-      <Switch>
+      <div>
+        {/* Navigation bar with links */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Routes>
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
         <Route exact path="/" component={AuctionList} />
         <Route path="/auction/:id" component={Auction} />
-      </Switch>
+      </Routes>
     </Router>
   );
 }
